12a5c37dda821cc9205d2430a1f2b7f80f8fcdfb
Mu <mumumumu0621@gmail.com>
2021-03-10 21:15:59 +0800

    finish localstorage

--- JsJuniorCourse/BMI/js/all.js
+++ JsJuniorCourse/BMI/js/all.js
@@ -8,10 +8,24 @@ const getResetImgDiv = getReset.children[2];
 let colorName =["_lightblue","_lightgreen","_orange1","_orange2","_red"];
 let calculateColor  ="";
 const getPromptText = document.querySelector('.promptText');
-const getContent = document.querySelector(".content>.container");
+const getContent = document.querySelector(".list");
+const getDelBtn = document.getElementById('allDel');
 
-
-
+getDelBtn.addEventListener('click',deleteLocal);
+let recordAry = [];
+function printLocalStorageBMI(){
+    getContent.innerHTML ='';
+    if(localStorage.getItem('item')!=null){
+        recordAry=JSON.parse(localStorage.getItem('item'));  
+        for (let i = 0; i <recordAry.length; i++){
+            printTemplate(recordAry[i].weight,recordAry[i].height,recordAry[i].BMIText,recordAry[i].BMI,recordAry[i].color,recordAry[i].year,recordAry[i].month,recordAry[i].date);
+        }
+    }
+    else{
+        return;
+    }
+}
+printLocalStorageBMI();
 
 getResult.addEventListener('click',lookResult);
 getResetImgDiv.addEventListener('click',goBack);
@@ -31,6 +45,7 @@ function lookResult() {
         getPromptText.classList.add("vh");
     }
     if(getHeight.value <=0 ||getWeight.value<=0){
+        getPromptText.textContent = "身高或體重格是錯誤";
         getPromptText.classList.remove("vh");
         return;
     }else{
@@ -38,6 +53,7 @@ function lookResult() {
     }
     let BMIValue =calculateUserBMI(getHeight.value,getWeight.value);
     let BMIText = "";
+    let dateObj = new Date();
     console.log(BMIValue);
     switch (true) {
         case (0<BMIValue &&BMIValue<18.5):
@@ -72,7 +88,12 @@ function lookResult() {
             break;
     }
     printResetOrResult(BMIValue,BMIText);
-    printTemplate(getWeight.value,getHeight.value,BMIText,BMIValue,calculateColor);
+    let todayAry =[];
+    todayAry = yearMonthDate(dateObj); //日期轉換成01-10-2020
+    recordAry.push(buildBMIObj(todayAry[2],todayAry[1],todayAry[0],BMIValue,BMIText,getWeight.value,getHeight.value,todayAry[3],calculateColor));
+    localStorage.setItem('item', JSON.stringify(recordAry));
+    printLocalStorageBMI();
+
 }
 function calculateUserBMI(height,weight){
     height=height/100;
@@ -89,22 +110,36 @@ function toggleColor(color){
     getResetImgDiv.classList.toggle("background_color"+color);
     getReset.classList.toggle("border_color"+color);
 }
-
-
+function yearMonthDate(dateObj){
+    let thisMonth = (dateObj.getMonth()+1);
+    thisMonth = thisMonth<10 ? ("0"+thisMonth):thisMonth;
+    let thisYear = dateObj.getFullYear();
+    let thisDate = dateObj.getDate();
+    thisDate = thisDate<10 ? ("0"+thisDate):thisDate;
+    let array = [thisDate, thisMonth, thisYear,dateObj.getTime()];
+    return array;
+}
+function buildBMIObj(y,m,d,bmi,bmitext,w,h,time,c){
+    let BMIObj = {
+        year:y,
+        month:m,
+        date:d,
+        BMI:bmi,
+        BMIText:bmitext,
+        weight:w,
+        height:h,
+        getTime:time,
+        color:c
+    }
+    return BMIObj;
+}
 function printResetOrResult(value,text){
     getReset.children[0].textContent = value;
     getBMIText.textContent=text;
     toggleResetResult();
     toggleColor(calculateColor);
 }
-function printTemplate(weight,height,BMIText,BMI,color){
-      // get today and depending on the circumstance add zero
-      let dateObj = new Date();
-      let thisMonth = (dateObj.getMonth()+1);
-      thisMonth = thisMonth<10 ? ("0"+thisMonth):thisMonth;
-      let thisYear = dateObj.getFullYear();
-      let thisDate = dateObj.getDate();
-      thisDate = thisDate<10 ? ("0"+thisDate):thisDate;
+function printTemplate(weight,height,BMIText,BMI,color,thisYear,thisMonth,thisDate){
       let recorderTemplate = `
   <ul class="pl-1 pr-1 border_left_color${color}">
   <li>${BMIText}</li>
@@ -118,4 +153,14 @@ function printTemplate(weight,height,BMIText,BMI,color){
 function goBack(){
     printResetOrResult();
     calculateColor  ="";
+}
+function deleteLocal(){
+    if(localStorage.getItem('item')){
+        recordAry = [];
+        localStorage.removeItem('item');
+    }
+    else{
+        return;
+    }
+printLocalStorageBMI();
 }
